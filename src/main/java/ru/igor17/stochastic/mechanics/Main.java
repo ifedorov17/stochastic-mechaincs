2f1a71c6c6fe4c5965f2d601197109837924d964
Igor Fedorov <igor170701@gmail.com>
2025-04-27 20:59:05 +0300

    Initial conditions

--- src/main/java/ru/igor17/stochastic/mechanics/Main.java
+++ src/main/java/ru/igor17/stochastic/mechanics/Main.java
@@ -1,9 +1,20 @@
 package ru.igor17.stochastic.mechanics;
 
+import java.util.List;
+import java.util.stream.Stream;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.print("Mechanics!!!");
+        System.out.print("Mechanics!!!\n\n");
+
+        //Начальные условия
+        int N = 9;
+        int k = 6;
+        List<Double> t = Stream.of(6.0, 6.0, 1.0, 1.0, 3.5, 3.5, 6.0, 3.5, 1.0).toList();
+        List<Double> T = Stream.of(145.0, 70.0, 145.0, 70.0, 107.5, 145.0, 107.5, 70.0, 107.5).toList();
+        List<Double> y = Stream.of(72.0, 63.0, 57.0, 49.0, 61., 67., 64., 56., 52.).toList();
+
     }
 
 }
