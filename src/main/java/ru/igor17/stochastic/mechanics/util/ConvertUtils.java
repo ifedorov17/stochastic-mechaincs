601bbfc8f58a3d1e4381b67507b6f6c20b2dde4b
Igor Fedorov <igor170701@gmail.com>
2025-04-27 23:18:52 +0300

    Estimate vector

--- src/main/java/ru/igor17/stochastic/mechanics/util/ConvertUtils.java
+++ src/main/java/ru/igor17/stochastic/mechanics/util/ConvertUtils.java
@@ -1,5 +1,10 @@
 package ru.igor17.stochastic.mechanics.util;
 
+import org.apache.commons.math3.linear.Array2DRowRealMatrix;
+import org.apache.commons.math3.linear.ArrayRealVector;
+import org.apache.commons.math3.linear.RealMatrix;
+import org.apache.commons.math3.linear.RealVector;
+
 import java.util.List;
 
 public class ConvertUtils {
@@ -8,6 +13,19 @@ public class ConvertUtils {
         return doubleList.stream()
                 .mapToDouble(Double::doubleValue)
                 .toArray();
+    }
+
+    public static RealMatrix toVectorColumnMatrix(RealVector vector) {
+        final int dim = vector.getDimension();
+        double[][] data = new double[dim][1];
+        for (int i = 0; i < dim; ++i) {
+            data[i][0] = vector.getEntry(i);
+        }
+        return new Array2DRowRealMatrix(data, false);
+    }
+
+    public static RealVector toRealVector(List<Double> vector) {
+        return new ArrayRealVector(toDoubleArray(vector));
     }
 
 }
