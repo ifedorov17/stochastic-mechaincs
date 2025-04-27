26eff035fa7bcd2ac30e2f47f6b5d49768d04f12
Igor Fedorov <igor170701@gmail.com>
2025-04-27 21:11:03 +0300

    Printing

--- src/main/java/ru/igor17/stochastic/mechanics/Main.java
+++ src/main/java/ru/igor17/stochastic/mechanics/Main.java
@@ -11,10 +11,24 @@ public class Main {
         //Начальные условия
         int N = 9;
         int k = 6;
-        List<Double> t = Stream.of(6.0, 6.0, 1.0, 1.0, 3.5, 3.5, 6.0, 3.5, 1.0).toList();
-        List<Double> T = Stream.of(145.0, 70.0, 145.0, 70.0, 107.5, 145.0, 107.5, 70.0, 107.5).toList();
-        List<Double> y = Stream.of(72.0, 63.0, 57.0, 49.0, 61., 67., 64., 56., 52.).toList();
+        final List<Double> t = Stream.of(6.0, 6.0, 1.0, 1.0, 3.5, 3.5, 6.0, 3.5, 1.0).toList();
+        final List<Double> T = Stream.of(145.0, 70.0, 145.0, 70.0, 107.5, 145.0, 107.5, 70.0, 107.5).toList();
+        final List<Double> y = Stream.of(72.0, 63.0, 57.0, 49.0, 61., 67., 64., 56., 52.).toList();
 
+        System.out.println("N: " + N);
+        System.out.println("K: " + k);
+
+        vectorPrint("t", t);
+        vectorPrint("T", T);
+        vectorPrint("y", y);
+
+        //Переход к безразмерным переменным
+
+
+    }
+
+    private static void vectorPrint(String explanation, final List<Double> vector) {
+        System.out.printf("%s: %s%n", explanation, vector.toString());
     }
 
 }
